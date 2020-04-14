211fe604577ec0d199e2c2d6ce578242f8408b5c
leye20 <leyeagbo@gmail.com>
2020-04-13 22:36:19 -0600

    incorrect logic added

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
 export interface IWelcomeProps {
-
 }
 
-export interface IWelcomeState {
+interface IWelcomeState {
     firstName: string,
     lastName: string,
     userName: string,
     password: string
 }
 
-export default class Welcome extends React.Component<IWelcomeProps, IWelcomeState, any> {
-    constructor ( props: string | boolean ) {
+export default class Welcome extends React.Component<IWelcomeProps, IWelcomeState> {
+    constructor ( props: string ) {
         super(props);
         this.state = {
-            firstName: "",
-            lastName: "",
+            firstName: "Dwayne",
+            lastName: "Jones",
             userName: "",
             password: ""
         }
@@ -24,47 +23,57 @@ export default class Welcome extends React.Component<IWelcomeProps, IWelcomeStat
     updateWelcome = ( event: any ) => {
         event.preventDefault();
 
-        let userNameString: string = "olaleye",
-            passwordString: string = "mayWorks123"
+        const userNameString: string = "olaleye";
+        const passwordString: string = "xxxx";
         
-        const userName: HTMLInputElement | null = document.querySelector( '[name="userName"]' );
-        const password: HTMLInputElement | null = document.querySelector( '[name="password"]' );
+        const inputUserName: HTMLInputElement | null = document.querySelector( '[name="userName"]' );
+        const inputPassword: HTMLInputElement | null = document.querySelector( '[name="password"]' );
 
-        if ( userName === null )
-            userNameString = '';
-        else
-            userNameString = userName.value;
-
-        if ( password === null )
-            passwordString = '';
-        else
-            passwordString = password.value;
-
-        this.setState( {
-            userName: userNameString,
-            password: passwordString,
-           } );
-        }
-    onClickLogin = () => {
-        this.setState( {
-            userName: this.state.userName,
-            password: this.state.password,
+        if ( inputUserName?.value === userNameString && inputPassword?.value === passwordString ) {
+            // correct logic
             
-            } );
-        }
+        } else {
+            // incorrect logic
+            this.setState( {
+                    userName: "incorrect username",
+                    password: "incorrect details" 
 
+                   } );
+                }
+            }
+            
+        
+        // userNameString = '';
+        // else
+        //     userNameString = userName.value;
+
+        // if ( password === null )
+        //     passwordString = '';
+        // else
+        //     passwordString = password.value;
+
+        // this.setState( {
+        //     userName: userNameString,
+        //     password: passwordString
+        //    } );
+        // }
+    
     render() {
         return (
-            <div>
+            <div className="forms">
                 <h2>{this.state.firstName} {this.state.lastName}</h2>
-                <p>{this.state.userName} That</p>
+                <h4>{this.state.userName}</h4>
                 <p>{this.state.password}</p>
-                <form>
+                <form onSubmit={this.updateWelcome}>
                     <h3>Welcome!</h3>
+                    <label htmlFor="firstName">firstName:</label>
+                    <input type="text" name="firstName" defaultValue={this.state.firstName} />
+                    <label htmlFor="lastName">lastName:</label>
+                    <input type="text" name="lastName" defaultValue={this.state.lastName} />
                     <label htmlFor="username">username:</label>
                     <input type="text" name="username" defaultValue={this.state.userName} />
                     <label htmlFor="password">password:</label>
-                    <input type="text" name="password" defaultValue={this.state.password} />
+                    <input type="password" name="password" defaultValue={this.state.password} />
                     <input type="submit" value="Login" />
                 </form>
             </div>
